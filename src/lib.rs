9500951a46d290d044a454e808fefc8d849d07df
41bfd856 f01a0573 <41bfd856@gmail.com>
2018-01-21 13:45:54 +0100

    Add constant and symbol

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,39 @@
+#[derive(Debug, PartialEq)]
+pub enum Term {
+    Constant(usize),
+    Symbol(usize),
+}
+
+impl Term {
+    pub fn differentiate(mut self, symbol: usize) -> Self {
+        match self {
+            Term::Constant(_) => self = Term::Constant(0),
+            Term::Symbol(other) => {
+                if other == symbol {
+                    self = Term::Constant(1);
+                }
+                else {
+                    self = Term::Constant(0);
+                }
+            }
+        }
+        self
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::Term;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn constant() {
+        assert_eq!(Term::Constant(1).differentiate(0), Term::Constant(0));
+        assert_eq!(Term::Constant(1).differentiate(1), Term::Constant(0));
+    }
+
+    #[test]
+    fn symbol() {
+        assert_eq!(Term::Symbol(0).differentiate(0), Term::Constant(1));
+        assert_eq!(Term::Symbol(0).differentiate(1), Term::Constant(0));
     }
 }
