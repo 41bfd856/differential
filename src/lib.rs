3e4cd186b7c969914b770571bdfea523ab4fd6b7
41bfd856 f01a0573 <41bfd856@gmail.com>
2018-01-28 17:31:53 +0100

    Add sum and move diff operations into seperate fns

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,34 @@
+pub type Constant = i32;
+pub type Sum = Vec<Term>;
+pub type Symbol = i32;
+
 #[derive(Debug, PartialEq)]
 pub enum Term {
-    Constant(usize),
-    Symbol(usize),
+    Constant(Constant),
+    Symbol(Symbol),
+    Sum(Sum),
 }
 
 impl Term {
-    pub fn differentiate(mut self, symbol: usize) -> Self {
+    pub fn differentiate(self, symbol: Symbol) -> Self {
         match self {
-            Term::Constant(_) => self = Term::Constant(0),
-            Term::Symbol(other) => {
-                if other == symbol {
-                    self = Term::Constant(1);
-                }
-                else {
-                    self = Term::Constant(0);
-                }
-            }
+            Term::Constant(_) => Term::Constant(0),
+            Term::Symbol(other) => diff_sym(other, symbol),
+            Term::Sum(sum) => diff_sum(sum, symbol),
         }
-        self
+    }
+}
+
+fn diff_sum(sum: Sum, symbol: Symbol) -> Term {
+    let sum = sum.into_iter().map(|i| i.differentiate(symbol)).collect();
+    Term::Sum(sum)
+}
+
+fn diff_sym(sym: Symbol, dsym: Symbol) -> Term {
+    if sym == dsym {
+        Term::Constant(1)
+    } else {
+        Term::Constant(0)
     }
 }
 
@@ -27,13 +38,24 @@ mod tests {
 
     #[test]
     fn constant() {
-        assert_eq!(Term::Constant(1).differentiate(0), Term::Constant(0));
-        assert_eq!(Term::Constant(1).differentiate(1), Term::Constant(0));
+        let s = 0;
+        assert_eq!(Term::Constant(1).differentiate(s), Term::Constant(0));
+        assert_eq!(Term::Constant(1).differentiate(s), Term::Constant(0));
     }
 
     #[test]
     fn symbol() {
-        assert_eq!(Term::Symbol(0).differentiate(0), Term::Constant(1));
-        assert_eq!(Term::Symbol(0).differentiate(1), Term::Constant(0));
+        let x = 0;
+        let y = 1;
+        assert_eq!(Term::Symbol(x).differentiate(x), Term::Constant(1));
+        assert_eq!(Term::Symbol(y).differentiate(x), Term::Constant(0));
+    }
+
+    #[test]
+    fn sum() {
+        let x = 0;
+        let sum = Term::Sum(vec![Term::Constant(1), Term::Constant(2)]);
+        let res = Term::Sum(vec![Term::Constant(0), Term::Constant(0)]);
+        assert_eq!(sum.differentiate(x), res);
     }
 }
